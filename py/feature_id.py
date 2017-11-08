fce55bd48003c1cb657799cd76d93f1e5cdec1e3
Nathaniel A Douglass <nate.a.douglass@gmail.com>
2017-11-08 02:13:53 -0800

    assigned id's to points

--- py/feature_id.py
+++ py/feature_id.py
@@ -29,12 +29,13 @@ logging.basicConfig(level=logging.INFO)
 logging.info("\t Starting script...")
 
 # File paths for data
-inpath = os.path.expanduser("~/HSUWebMap/Spatial_Data/Buildings_Nov122016_3.js")
-#bldng_label_path = os.path.expanduser("~/HSUWebMap/Spatial_Data/BuildingLabels_Dec20_2016.js")
-#bldng_name_path = os.path.expanduser("~/HSUWebMap/Spatial_Data/BuildingNames8.js")
-#bldng_phrase_list_path = os.path.expanduser("~/HSUWebMap/js/BuildingPhraseList.js")
+#inpath = os.path.expanduser("~/HSUWebMap/Spatial_Data/Buildings_Nov122016_3.js")
+#inpath = os.path.expanduser("~/HSUWebMap/Spatial_Data/BuildingLabels_Dec20_2016.js")
+#inpath = os.path.expanduser("~/HSUWebMap/Spatial_Data/BuildingNames8.js")
+#inpath = os.path.expanduser("~/HSUWebMap/js/BuildingPhraseList.js")
+inpath = os.path.expanduser("~/HSUWebMap/Spatial_Data/Points_Dec9_2016.js")
 
-outpath = os.path.expanduser("~/HSUWebMap/Spatial_Data/Tmp_Buildings_Nov122016_3.js")
+outpath = os.path.expanduser("~/HSUWebMap/Spatial_Data/temp.js")
 
 # File path to id sheet
 id_sheet = os.path.expanduser("~/HSUWebMap/id_sheet.csv")
@@ -65,7 +66,6 @@ with open(id_sheet, 'r') as inf:
         else:
             logging.info("\tid taken")
             pass
-logging.info("\t--------------------------------------\r")
 
 fdata = open(inpath, 'r+')
 data = json.load(fdata)
@@ -74,7 +74,10 @@ out = open(outpath, 'a')
 
 for feature in data['features']:
     props = feature['properties']
-    name = props['Name']
+    if props.has_key('Name'):
+        name = props['Name']
+    else:
+        name = props['NAME']
     print(feature)
     if not props.has_key("id"):
         logging.info("\t\tDoes NOT have id property \t") 
@@ -100,7 +103,9 @@ with open(id_sheet, 'r') as inf, open(temp, 'w') as outf:
         id = row.get('id', None)
         in_use = row.get('in_use', None)
         name = row.get('name', None)
-        if id == used_id_list[id_count]:
+        if in_use == 'Y':
+            outf.write(id + ',' + in_use + ',' + name + '\n')
+        elif id == used_id_list[id_count]:
             outf.write(id + ',' + 'Y' + ',' + name_list[id_count] + '\n')
             if id_count >= (len(used_id_list)-1):
                 pass
